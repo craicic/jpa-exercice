879304cbe244e7a9f11d9384f13155247fd48bdb
xxjokerx <rouzic.jeremie@gmail.com>
2021-02-12 12:50:30 +0100

    add a sql file to insert fake data into database

---
+++ src/test/resources/sql/auto_generated.sql
@@ -0,0 +1,37 @@
+TRUNCATE table board_game_theme restart identity CASCADE;
+TRUNCATE table theme restart identity CASCADE;
+TRUNCATE table board_game restart identity CASCADE;
+TRUNCATE table publisher restart identity CASCADE;
+
+/*
+
+*/
+CREATE OR REPLACE FUNCTION random_between(low INT ,high INT)
+    RETURNS INT AS
+$$
+BEGIN
+    RETURN floor(random()* (high-low + 1) + low);
+END;
+$$ language 'plpgsql' STRICT;
+
+INSERT INTO publisher (id, name, website)
+SELECT seq, 'publisher_' || seq, 'www.' || MD5(random()::text) || '.com'
+FROM generate_series(1, 10) AS seq;
+
+INSERT INTO theme (id, name, short_desc)
+SELECT seq, 'theme_' || seq, 'This is a short desc...' || MD5(random()::text)
+FROM generate_series(11, 20) AS seq;
+
+INSERT INTO board_game(id, description, min_age, name, fk_publisher)
+SELECT seq,
+       'This is a desc...' || MD5(random()::text),
+       random()::int,
+       'title' || seq || MD5(random()::text),
+       random_between(1,10)
+FROM generate_series(21, 1000) AS seq;
+
+INSERT INTO board_game_theme(fk_game, fk_theme)
+SELECT generate_series(21, 1000), random_between(11, 15);
+
+INSERT INTO board_game_theme(fk_game, fk_theme)
+SELECT generate_series(21, 1000), random_between(16, 20);
